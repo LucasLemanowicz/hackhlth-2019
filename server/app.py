b0547952f686f77baa590438e1ef45396aa5b905
Lucas Lemanowicz <lemanowiczlucas@gmail.com>
2019-10-27 03:01:28 -0700

    Finish SDH data implementation

--- server/app.py
+++ server/app.py
@@ -25,15 +25,19 @@ class FhirApi:
         return [patient.as_json() for patient in patients]
 
 class SdhApi:
+    FOODSTAMP_DATA_SOURCE= "https://nccd.cdc.gov/DHDSPAtlas/SocialDeterminantThematicDataHandler.ashx?{%22ThemeId%22:41,%22GeographyType%22:%22county%22,%22ParentGeographyFilter%22:%22%22,%22ThemeFilterOptions%22:[]}"
+    POVERTY_DATA_SOURCE  = "https://nccd.cdc.gov/DHDSPAtlas/SocialDeterminantThematicDataHandler.ashx?{%22ThemeId%22:52,%22GeographyType%22:%22county%22,%22ParentGeographyFilter%22:%22%22,%22ThemeFilterOptions%22:[]}"
+    PARK_ACCESS_DATA_SOURCE = "https://nccd.cdc.gov/DHDSPAtlas/SocialDeterminantThematicDataHandler.ashx?{%22ThemeId%22:61,%22GeographyType%22:%22county%22,%22ParentGeographyFilter%22:%22%22,%22ThemeFilterOptions%22:[]}"
     DIABETES_DATA_SOURCE = "https://nccd.cdc.gov/DHDSPAtlas/SocialDeterminantThematicDataHandler.ashx?{%22ThemeId%22:62,%22GeographyType%22:%22county%22,%22ParentGeographyFilter%22:%22%22,%22ThemeFilterOptions%22:[]}"
     OBESITY_DATA_SOURCE  = "https://nccd.cdc.gov/DHDSPAtlas/SocialDeterminantThematicDataHandler.ashx?{%22ThemeId%22:63,%22GeographyType%22:%22county%22,%22ParentGeographyFilter%22:%22%22,%22ThemeFilterOptions%22:[]}"
     INACTIVE_DATA_SOURCE = "https://nccd.cdc.gov/DHDSPAtlas/SocialDeterminantThematicDataHandler.ashx?{%22ThemeId%22:64,%22GeographyType%22:%22county%22,%22ParentGeographyFilter%22:%22%22,%22ThemeFilterOptions%22:[]}"
-
+    HOUSING_PROBLEM_DATA_SOURCE = "https://nccd.cdc.gov/DHDSPAtlas/SocialDeterminantThematicDataHandler.ashx?{%22ThemeId%22:74,%22GeographyType%22:%22county%22,%22ParentGeographyFilter%22:%22%22,%22ThemeFilterOptions%22:[]}"
     def __init__(self):
         self.sdh_data = None
 
     def warmup(self):
-        self._get_sdh_data()
+        if not self.sdh_data:
+            self._get_sdh_data()
 
     def reset(self):
         self.sdh_data = None
@@ -75,15 +79,55 @@ class SdhApi:
             'Inactive': d['Value'],
         } for d in dict_data]
 
+        # Poverty Data
+        dict_data = self._load_and_convert_table(self.POVERTY_DATA_SOURCE)
+        poverty_data = [{
+            'State': d['State'],
+            'County': d['County'],
+            'Poverty': d['Value'],
+        } for d in dict_data]
+
+        # Foodstamp / SNAP Data
+        dict_data = self._load_and_convert_table(self.FOODSTAMP_DATA_SOURCE)
+        foodstamp_data = [{
+            'State': d['State'],
+            'County': d['County'],
+            'Foodstamp': d['Value'],
+        } for d in dict_data]
+
+        # Park Access Data
+        dict_data = self._load_and_convert_table(self.PARK_ACCESS_DATA_SOURCE)
+        park_access_data = [{
+            'State': d['State'],
+            'County': d['County'],
+            'ParkAccess': d['Value'],
+        } for d in dict_data]
+
+        # Severe Housing Problem Data
+        dict_data = self._load_and_convert_table(self.HOUSING_PROBLEM_DATA_SOURCE)
+        housing_data = [{
+            'State': d['State'],
+            'County': d['County'],
+            'SevereHousingShortage': d['Value'],
+        } for d in dict_data]
+
         # Convert to single table
         diabetes = pd.DataFrame(diabetes_data)
         obesity  = pd.DataFrame(obesity_data)
         inactive = pd.DataFrame(inactive_data)
+        poverty  = pd.DataFrame(poverty_data)
+        foodstamp = pd.DataFrame(foodstamp_data)
+        park_access = pd.DataFrame(park_access_data)
+        housing  = pd.DataFrame(housing_data)
 
         part1 = pd.merge(obesity, diabetes, on=['County', 'State'])
-        part2 = pd.merge(part1, inactive, on=['County', 'State'])
+        part2 = pd.merge(inactive, poverty, on=['County', 'State'])
+        part3 = pd.merge(park_access, foodstamp, on=['County', 'State'])
+        part4 = pd.merge(part1, part2, on=['County', 'State'])
+        part5 = pd.merge(part3, part4, on=['County', 'State'])
+        part6 = pd.merge(part5, housing, on=['County', 'State'])
 
-        self.sdh_data = part2.to_dict(orient='records')
+        self.sdh_data = part6.to_dict(orient='records')
 
     def _load_and_convert_table(self, url):
         http = requests.get(url)
@@ -106,6 +150,10 @@ def hello_world():
 @app.route('/warmup')
 def warmup():
     sdh_api.warmup()
+
+@app.route('/reset')
+def reset():
+    sdh_api.reset()
 
 @app.route('/fhir/patients')
 def patient():
