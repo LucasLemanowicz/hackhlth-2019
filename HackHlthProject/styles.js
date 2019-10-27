07ff8107b6af55adc1c0b124c1b389a1620962d2
Kayo <kayo@Kayos-MacBook.local>
2019-10-27 11:02:23 -0700

    Tweak

--- HackHlthProject/styles.js
+++ HackHlthProject/styles.js
@@ -65,7 +65,7 @@ const styles = StyleSheet.create({
     todayGoals: {
         paddingTop: 10,
         paddingLeft: 15,
-        fontSize: 16,
+        fontSize: 18,
         fontWeight: "600",
         alignSelf: "center"
     },
