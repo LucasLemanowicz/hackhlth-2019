b717340dd5069cce5831bfcc134e61abd456c272
Lucas Lemanowicz <lemanowiczlucas@gmail.com>
2019-10-26 23:29:47 -0700

    Add FHIR integration for patients

--- server/app.py
+++ server/app.py
@@ -1,14 +1,34 @@
 import os
 
-from flask import Flask
+from clientpy4.fhirclient import client
+from clientpy4.fhirclient.models import patient as p
+from flask import Flask, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
+class FhirApi:
+    def __init__(self, api_base='http://test.fhir.org/r4'):
+        self.smart = client.FHIRClient(settings={
+            'app_id': 'hackhlth2019',
+            'api_base': api_base,
+        })
+
+    def patients(self):
+        search = p.Patient.where(struct={})
+        patients = search.perform_resources(self.smart.server)
+        return [patient.as_json() for patient in patients]
+
+fhir_api = FhirApi()
+
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/patients')
+def patient():
+    return jsonify(fhir_api.patients())
 
 if __name__ == '__main__':
     app.run(debug=True,
