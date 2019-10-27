27385d361b4ef214a044cea709b6a2d3bfc4a709
Kayo <kayo@Kayos-MacBook.local>
2019-10-27 04:21:01 -0700

    Add dummy chart

--- HackHlthProject/TrendsScreen.js
+++ HackHlthProject/TrendsScreen.js
@@ -1,16 +1,17 @@
-import {
-    LineChart,
-    BarChart,
-    PieChart,
-    ProgressChart,
-    ContributionGraph,
-    StackedBarChart
-  } from 'react-native-chart-kit'
-  import React from 'react';
+import React from 'react';
 import { SafeAreaView, ScrollView, Text, View } from 'react-native';
-import { CheckBox } from "react-native-elements"
+import { LineChart } from 'react-native-chart-kit';
 import { styles } from "./styles";
 
+const linedata = {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+    datasets: [
+        {
+            data: [20, 45, 28, 80, 99, 43],
+            strokeWidth: 2, // optional
+        },
+    ],
+};
 class TrendsScreen extends React.Component {
     render() {
         return (
@@ -20,9 +21,30 @@ class TrendsScreen extends React.Component {
                     style={styles.scrollView}>
                     <View style={styles.healthScreen}>
                         <Text style={styles.healthTitle}>Trends</Text>
-                        <Text>My Data Shared With:</Text>
-                        <CheckBox containerStyle={styles.normalizedCheckbox} title="Dr. John" />
-                        <CheckBox containerStyle={styles.normalizedCheckbox} title="Walmart Inc." />
+                        <Text>
+                            Bezier Line Chart
+                        </Text>
+                        <LineChart
+                            data={linedata}
+                            width={320}
+                            height={240}
+                            yAxisLabel={'$'}
+                            chartConfig={{
+                                backgroundColor: '#e26a00',
+                                backgroundGradientFrom: '#fb8c00',
+                                backgroundGradientTo: '#ffa726',
+                                decimalPlaces: 2, // optional, defaults to 2dp
+                                color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                                style: {
+                                    borderRadius: 16
+                                }
+                            }}
+                            bezier
+                            style={{
+                                borderRadius: 16
+                            }}
+                        />
+
                     </View>
                 </ScrollView>
             </SafeAreaView>
@@ -31,3 +53,4 @@ class TrendsScreen extends React.Component {
 }
 
 export { TrendsScreen };
+
